81198d32cf168c99b04da75dc08beb073da35f43
TYL@KL <aikawa362@gmail.com>
2025-08-14 08:54:33 +0800

    [what] Sample Project for App Verify Windows display scaling issue.

---
+++ ResizeProject/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using ResizeProject.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ResizeProject
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                if (DataContext is MainViewModel viewModel)
+                {
+                    if (viewModel.IsShowDetail == true)
+                    {
+                        viewModel.IsShowDetail = false;
+
+                    }
+                }
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Maximized;
+        }
+    }
+}
---
+++ ResizeProject/ViewModels/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResizeProject.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+
+        public MainViewModel() {
+            AddNewRecordCommand = new RelayCommand(OnAddNewRecord);
+        
+        }
+
+        private bool _isshowdetail;
+        public bool IsShowDetail
+        {
+            get => _isshowdetail;
+            set => SetProperty(ref _isshowdetail, value);
+
+        }
+
+        public IRelayCommand AddNewRecordCommand { get; }
+        private void OnAddNewRecord()
+        {
+            IsShowDetail = true; 
+        }
+    }
+}
